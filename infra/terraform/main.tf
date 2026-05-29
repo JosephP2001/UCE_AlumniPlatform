2101cb18409aec1e27f1e9b3ef79fd71fdd3caa8
JosephP2001 <josephponce600@outlook.com>
2026-05-29 18:41:46 -0500

    infra: fix Docker repository to use noble for Ubuntu 26.04 compatibility

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -71,6 +71,34 @@ resource "aws_route_table_association" "public_1a" {
 }
 
 # ─────────────────────────────────────────
+# NAT GATEWAY  <------------JSUTO FOR TESTTING
+# ─────────────────────────────────────────
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1a.id
+  tags = { Name = "uce-qa-nat" }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = { Name = "uce-qa-private-rt" }
+}
+
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+# ─────────────────────────────────────────
 # SECURITY GROUPS
 # ─────────────────────────────────────────
 resource "aws_security_group" "sg_bastion" {
@@ -164,7 +192,7 @@ resource "aws_instance" "qa_auth_jobs" {
     install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     chmod a+r /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu noble stable"
     apt update -y
     apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
     systemctl enable docker
